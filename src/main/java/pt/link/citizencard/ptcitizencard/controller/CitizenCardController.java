9e0ddb207e5d272744131688e11ad103ece3ed54
Miguel Pinto <miguelpinto25@hotmail.com>
2016-07-26 15:29:35 +0100

    Update interface

--- src/main/java/pt/link/citizencard/ptcitizencard/controller/CitizenCardController.java
+++ src/main/java/pt/link/citizencard/ptcitizencard/controller/CitizenCardController.java
@@ -1,5 +1,7 @@
 package pt.link.citizencard.ptcitizencard.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import pt.link.citizencard.ptcitizencard.cardreader.CitizenCard;
 import pt.link.citizencard.ptcitizencard.domain.User;
 import pt.link.citizencard.ptcitizencard.service.impl.CitizenCardServiceImpl;
+import pt.link.libraries.linktables.ui.ColumnsProperties;
+import pt.link.libraries.linktables.ui.LinkTable;
+import pt.link.libraries.linktables.ui.TablePopulator;
+import pt.link.libraries.pagging.Page;
+import pt.link.libraries.pagging.PageInfo;
 import pteidlib.PteidException;
 
 @Controller
@@ -45,8 +52,20 @@ public class CitizenCardController{
 	public String verify(Model model, User user){
 		loadCC();
 		try {
-			if(cc.verifyPin(user.getPassword()))
+			if(cc.verifyPin(user.getPassword())){
+//				LinkTable<CitizenCard> ccs = new LinkTable<CitizenCard>("tableCC");
+//				ccs.setColumnsNames(new String[]{"cardNumber", "birthDate", "country", "firstname", "nationality"});
+//				ccs.addColumnProperties("cardNumber", new ColumnsProperties("cardNumber", "Card Number"));
+//				ccs.addColumnProperties("birthDate", new ColumnsProperties("birthDate", "Birth Date"));
+//				ccs.addColumnProperties("country", new ColumnsProperties("country", "Country"));
+//				ccs.addColumnProperties("firstname", new ColumnsProperties("firstname", "Name"));
+//				ccs.addColumnProperties("nationality", new ColumnsProperties("nationality", "Nationality"));
+//				ccs.setTablePopulator(new CCInfoTablePopulator());
+//				ccs.setIdFieldName("cardNumber");
+//				model.addAttribute("citizenCard", ccs);
+				
 				model.addAttribute("citizenCard", ccService.listCitizenCards());
+			}
 			else
 				return "cclogin";
 		} catch (PteidException e) {
@@ -66,4 +85,21 @@ public class CitizenCardController{
 		ccService.loadCC();
 		cc = ccService.getCC();
 	}
+	
+	public class CCInfoTablePopulator extends TablePopulator<CitizenCard>{
+		
+		public CCInfoTablePopulator(){
+			super();
+		}
+		
+		@Override
+		public Page<CitizenCard> getPage(int offset, int pageSize, String sortColumnName,
+				String sortOrder) {
+			PageInfo pageInfo = new PageInfo(offset, pageSize, sortColumnName, sortOrder);
+			List<CitizenCard> ccs = ccService.listCitizenCards();
+			return Page.createPageFromFullResults(ccs, pageInfo);
+		}
+	}
+
+		
 }
--- src/main/webapp/WEB-INF/jsp/list.jsp
+++ src/main/webapp/WEB-INF/jsp/list.jsp
@@ -2,12 +2,18 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib uri="http://pt.link.libraries/linktables" prefix="linktable"%>
 
 <!DOCTYPE html>
 <html>
 
 <body>
 
+	<%-- <spring:url value="/list.html" var="urlExemplo">
+	</spring:url> 
+
+	<linktable:table id="citizenCard" tableObject="${citizenCard}" currentUrl="${urlExemplo}">
+	</linktable:table> --%>
 	<div class="container">
 
 		<h1>Citizen Card Info</h1>
